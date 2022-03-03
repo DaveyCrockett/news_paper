83b8653007876fa4b636571f694b58404112466f
augustspies22 <augustspies22@gmail.com>
2022-03-02 17:33:05 -0800

    changes

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["news-article-paper.herokuapp.com"
+ALLOWED_HOSTS = ["news-article-paper.herokuapp.com",
+                'localhost',
                 '127.0.0.1']
 
 
@@ -82,10 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.dj_url("DATABASE_URL")
 }
 
 
